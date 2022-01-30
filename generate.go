62c351bb64e6ad5b02f40d481b6f2a2edf10e939
nheyn <nick@heyn.io>
2022-01-30 16:49:24 -0700

    progrium/sveltish#4 Update the Parse and GenerateJS functions to use new html ast

--- generate.go
+++ generate.go
@@ -21,43 +21,77 @@ func GenerateJS(c *Component) ([]byte, error) {
 } from`, s.Str("./runtime"))
 	s.Line("")
 	s.Func("create_fragment", []string{"ctx"}, func(s *js.Source) {
-		for _, node := range flatten(c.HTML) {
-			s.Stmt("let", node.Tag)
-		}
+		WalkHTML(c.HTML, func(n HTMLNode, _ []HTMLNode) bool {
+			switch node := n.(type) {
+			case *ElNode:
+				s.Stmt("let", node.Tag)
+			}
+			return true
+		})
 		s.Line("")
 		s.Stmt("return", func(s *js.Source) {
 			s.Stmt("c()", func(s *js.Source) {
-				for _, node := range flatten(c.HTML) {
-					s.Stmt(node.Tag, "=", fmt.Sprintf(`element("%s")`, node.Tag))
-					if node.Text != "" {
-						s.Stmt(s.Chain(node.Tag, "textContent"), "=", fmt.Sprintf("`%s`", strings.Replace(node.Text, "{", "${", -1)))
+				WalkHTML(c.HTML, func(n HTMLNode, _ []HTMLNode) bool {
+					switch node := n.(type) {
+					case *ElNode:
+						s.Stmt(node.Tag, "=", fmt.Sprintf(`element("%s")`, node.Tag))
+						if len(node.Els) == 1 {
+							switch child := node.Els[0].(type) {
+							case *TxtNode:
+								s.Stmt(
+									s.Chain(node.Tag, "textContent"),
+									"=",
+									fmt.Sprintf("`%s`", strings.Replace(child.Content, "{", "${", -1)),
+								)
+								return false
+							}
+						}
 					}
-				}
+					return true
+				})
 			}, ",")
 			s.Stmt("m(target, anchor)", func(s *js.Source) {
-				for _, node := range c.HTML.Children {
-					s.Stmt(s.Call("insert", "target", node.Tag, "anchor"))
-					walk(node, func(nn *Node) bool {
-						if nn != node {
-							s.Stmt(s.Call("append", nn.Parent.Tag, nn.Tag))
+				WalkHTML(c.HTML, func(n HTMLNode, ps []HTMLNode) bool {
+					switch node := n.(type) {
+					case *ElNode:
+						s.Stmt(s.Call("insert", "target", node.Tag, "anchor"))
+
+						if len(ps) > 1 {
+							switch parent := ps[0].(type) {
+							case *ElNode:
+								s.Stmt(s.Call("append", parent.Tag, node.Tag))
+							}
 						}
-						return true
-					})
-				}
+					}
+					return true
+				})
 			}, ",")
 			s.Line("p: noop,")
 			s.Line("i: noop,")
 			s.Line("o: noop,")
 			s.Stmt("d(detaching)", func(s *js.Source) {
-				for _, node := range c.HTML.Children {
-					s.Stmt("if (detaching)", s.Call("detach", node.Tag))
-				}
+				WalkHTML(c.HTML, func(n HTMLNode, _ []HTMLNode) bool {
+					switch node := n.(type) {
+					case *ElNode:
+						s.Stmt("if (detaching)", s.Call("detach", node.Tag))
+					}
+					return true
+				})
 			})
 
 		})
 	})
 	s.Line("")
-	s.Line(c.JS.Children[0].Text)
+	WalkHTML(c.JS, func(n HTMLNode, _ []HTMLNode) bool {
+		switch node := n.(type) {
+		case *LeafElNode:
+			s.Line(node.Content)
+		}
+		return true
+	})
+	if len(c.JS.Roots) > 0 {
+
+	}
 	s.Line("")
 	s.Stmt("class", c.Name, "extends SvelteComponent", func(s *js.Source) {
 		s.Stmt("constructor(options)", func(s *js.Source) {
--- parse.go
+++ parse.go
@@ -1,70 +1,242 @@
 package sveltish
 
 import (
+	"errors"
 	"io"
-	"strings"
+	//"strings"
 
 	"github.com/tdewolff/parse/v2"
 	"github.com/tdewolff/parse/v2/html"
+
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func Parse(src io.Reader, name string) (*Component, error) {
-	l := html.NewLexer(parse.NewInput(src))
-	loop := true
-	root := &Node{}
-	curr := root
-	var last *Node
-	for loop {
-		tt, data := l.Next()
-		switch tt {
-		case html.ErrorToken:
-			err := l.Err()
-			if err != io.EOF {
-				return nil, err
-			}
-			loop = false
-			break
-		case html.StartTagToken:
-			tag := string(data[1:])
-			node := &Node{Tag: tag, Parent: curr}
-			curr.Children = append(curr.Children, node)
-			last = curr
-			curr = node
-			break
-		case html.EndTagToken:
-			curr = last
-			break
-		case html.TextToken:
-			curr.Text = strings.Trim(string(data), "\n\t ")
-			break
-		}
+	root := &RootNode{}
+	_, err := root.ParseHTML(html.NewLexer(parse.NewInput(src)))
+	if err != nil {
+		return nil, err
 	}
+
 	c := &Component{
 		Name: name,
-		JS:   &Node{},
-		HTML: &Node{},
-		CSS:  &Node{},
+		JS:   &RootNode{},
+		HTML: &RootNode{},
+		CSS:  &RootNode{},
 	}
-
-	allHTML := make(map[*Node]bool)
-	walk(root, func(n *Node) bool {
-		switch n.Tag {
-		case "script":
-			c.JS.Children = append(c.JS.Children, n)
-			break
-		case "style":
-			c.CSS.Children = append(c.CSS.Children, n)
-			break
-		default:
-			allHTML[n] = true
-			if _, exists := allHTML[n.Parent]; exists {
-				break
-			}
-
-			c.HTML.Children = append(c.HTML.Children, n)
+	WalkHTML(root, func(n HTMLNode, ps []HTMLNode) bool {
+		if len(ps) == 0 {
+			return true
 		}
-		return true
+		if ps[0] != root {
+			panic("Parser should not walk more than the roots")
+		}
+		if lf, ok := n.(*LeafElNode); ok {
+			switch lf.Tag {
+			case "script":
+				c.JS.AppendChild(lf)
+				return false
+			case "style":
+				c.CSS.AppendChild(lf)
+				return false
+			}
+		}
+
+		c.HTML.AppendChild(n)
+		return false
 	})
 
+	fmt.Println("-------------------")
+	spew.Dump(c)
+	fmt.Println("-------------------")
+
 	return c, nil
+}
+
+type HTMLParser interface {
+	ParseHTML(l HTMLLexer) (HTMLLexer, error)
+}
+
+func (n *RootNode) ParseHTML(l HTMLLexer) (HTMLLexer, error) {
+	var err error
+	for err == nil {
+		l, err = parseNextChild(l, n)
+	}
+
+	if err != io.EOF {
+		return l, err
+	}
+	return l, nil
+}
+
+func (n *ElNode) ParseHTML(l HTMLLexer) (HTMLLexer, error) {
+	tt, data := l.Next()
+	if tt != html.StartTagToken {
+		return l, errors.New("Invalid parser position passed to elNode.ParseHTML")
+	}
+	n.Tag = string(data[1:])
+
+	tt, data = l.Next()
+	for tt != html.StartTagCloseToken {
+		if tt != html.AttributeToken {
+			return l, errors.New("Invalid token when attribute expected")
+		}
+
+		//TODO, parse attributes
+		tt, data = l.Next()
+	}
+	tt, data = l.Next()
+
+	for tt != html.EndTagToken {
+		nextL, err := parseNextChild(wrapLexer(tt, data, l), n)
+		if err != nil {
+			return nextL, err
+		}
+
+		l = nextL
+		tt, data = l.Next()
+	}
+	return l, nil
+}
+
+func (n *LeafElNode) ParseHTML(l HTMLLexer) (HTMLLexer, error) {
+	tt, data := l.Next()
+	switch tt {
+	case html.SvgToken:
+		return l, errors.New("NYI: parsing <svg />")
+	case html.MathToken:
+		return l, errors.New("NYI: parsing <math />")
+	case html.StartTagToken:
+		n.Tag = string(data[1:])
+	default:
+		return l, errors.New("Invalid parser position passed to leafElNode.ParseHTML")
+	}
+
+	tt, data = l.Next()
+	for tt != html.StartTagCloseToken {
+		if tt != html.AttributeToken {
+			return l, errors.New("Invalid token when attribute expected")
+		}
+
+		//TODO, parse attributes
+		tt, data = l.Next()
+	}
+	tt, data = l.Next()
+
+	if tt == html.TextToken {
+		n.Content = string(data)
+		tt, data = l.Next()
+	}
+	if tt != html.EndTagToken {
+		return l, errors.New("Invalid parser token in leaf element")
+	}
+
+	return l, nil
+}
+
+func (n *TxtNode) ParseHTML(l HTMLLexer) (HTMLLexer, error) {
+	tt, data := l.Next()
+	if tt != html.TextToken {
+		return l, errors.New("Invalid parser position passed to txtNode.ParseHTML")
+	}
+
+	//TODO, parse {...} for content
+	n.Content = string(data)
+	return l, nil
+}
+
+func parseNextChild(l HTMLLexer, n HTMLNodeContainer) (HTMLLexer, error) {
+	tt, data := l.Next()
+	switch tt {
+	case html.StartTagToken:
+		var newNode interface {
+			HTMLParser
+			HTMLNode
+		}
+
+		tag := string(data[1:])
+		if tag == "script" || tag == "style" {
+			newNode = &LeafElNode{}
+		} else {
+			newNode = &ElNode{}
+		}
+
+		nextL, err := newNode.ParseHTML(wrapLexer(tt, data, l))
+		if err != nil {
+			return nextL, err
+		}
+
+		n.AppendChild(newNode)
+		return nextL, nil
+	case html.SvgToken:
+	case html.MathToken:
+		newNode := &LeafElNode{}
+		nextL, err := newNode.ParseHTML(wrapLexer(tt, data, l))
+		if err != nil {
+			return nextL, err
+		}
+
+		n.AppendChild(newNode)
+		return nextL, nil
+	case html.TextToken:
+		newNode := &TxtNode{}
+		nextL, err := newNode.ParseHTML(wrapLexer(tt, data, l))
+		if err != nil {
+			return nextL, err
+		}
+
+		n.AppendChild(newNode)
+		return nextL, nil
+	case html.CommentToken:
+		return l, nil
+	case html.ErrorToken:
+		return l, l.Err()
+	}
+
+	return l, errors.New("invalid token in children")
+}
+
+type HTMLLexer interface {
+	Next() (html.TokenType, []byte)
+	Err() error
+}
+
+type wrappedLexer struct {
+	tt        html.TokenType
+	data      []byte
+	canUnwrap bool
+	l         HTMLLexer
+}
+
+func wrapLexer(tt html.TokenType, data []byte, l HTMLLexer) *wrappedLexer {
+	if wl, ok := l.(*wrappedLexer); ok {
+		return &wrappedLexer{tt, data, false, wl.unwrap()}
+	}
+
+	return &wrappedLexer{tt, data, false, l}
+}
+
+func (l *wrappedLexer) unwrap() HTMLLexer {
+	if !l.canUnwrap {
+		return l
+	}
+	if innerL, ok := l.l.(*wrappedLexer); ok {
+		return innerL.unwrap()
+	}
+
+	return l.l
+}
+
+func (l *wrappedLexer) Next() (html.TokenType, []byte) {
+	if l.canUnwrap {
+		return l.l.Next()
+	}
+
+	l.canUnwrap = true
+	return l.tt, l.data
+}
+
+func (l *wrappedLexer) Err() error {
+	return l.l.Err()
 }
